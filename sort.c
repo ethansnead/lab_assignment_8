05dbb9e309a897ca4b7b938edc4d759873a86393
ethansnead <136924908+ethansnead@users.noreply.github.com>
2023-07-16 15:15:26 -0400

    Update sort.c

--- sort.c
+++ sort.c
@@ -4,17 +4,92 @@
 
 int extraMemoryAllocated;
 
+
+void heapHelper(int arr[], int n, int i){
+	int largest = i; 
+	int l = 2 * i + 1;
+	int r = 2 * i + 2;
+
+	if (l < n && arr[l] > arr[largest]) 
+		largest = l; 
+	if (r < n && arr[r] > arr[largest]) 
+		largest = r; 
+	if (largest != i) { 
+		swap(arr[i], arr[largest]); 
+
+		// Recursively heapify the affected sub-tree 
+		heapHelper(arr, n, largest); 
+	} 
+} 
+
+
+
 // implements heap sort
 // extraMemoryAllocated counts bytes of memory allocated
-void heapSort(int arr[], int n)
-{
+void heapSort(int arr[], int n){
+	extraMemoryAllocated += 4;
+
+	for (int i = n / 2 - 1; i >= 0; i--)
+		heapHelper(arr, n, i);
+
+	for(int i = n - 1; i >= 0; i--){
+		swap(arr[0], arr[i]);
+
+		heapHelper(arr, i, 0);
+	}
 }
 
 
-// implement merge sort
-// extraMemoryAllocated counts bytes of extra memory allocated
+void merge(int pData[], int l, int m, int r){
+int i, j, k;
+int n1 = m - l + 1;
+int n2 = r - m;
+
+int L[n1], R[n2];
+extraMemoryAllocated += (n1 + n2);
+
+for(i = 0; i < n1; i++)
+	L[i] = arr[l + i];
+for(j = 0; j < n2; j++)
+	R[j] = arr[m + 1 + j];
+
+i = 0;
+j = 0;
+k = l;
+
+while(i < n1 && j < n2){
+	if (L[i] <= R[j]{
+		arr[k] = L[i];
+		i++;
+	}
+	else{
+		arr[k] = R[j];
+		j++;
+	}
+	k++;
+}
+while (i < n1) { 
+	arr[k] = L[i]; 
+	i++; 
+	k++; 
+} 
+while (j < n2) { 
+	arr[k] = R[j]; 
+	j++; 
+	k++; 
+	} 
+} 
+
+
+
 void mergeSort(int pData[], int l, int r)
 {
+	if(l<r){
+		int m = l + (r-l) / 2;
+		mergeSort(pData, l, m);
+		mergeSort(pData, m + 1, r);
+		merge(pData, l, m, r);
+	}
 }
 
 // parses input file to an integer array
